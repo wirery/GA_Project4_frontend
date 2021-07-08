71aba7ac195b148adeb62eaba5780c4bcbb39c39
wirery <wire_ryan@yahoo.com>
2021-07-08 09:31:01 -0400

    app page started, state created

--- coin_exchange_app/src/App.js
+++ coin_exchange_app/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const baseURL = 'http://localhost:3003';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      country: [],
+      img: [],
+      year: [],
+      quantity: [],
+      grade: [],
+      value: [],
+
+    }
+  }
 }
-
-export default App;
